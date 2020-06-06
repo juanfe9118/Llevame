b7c1ccffce479fef8e0c86c0501c4adf209ff2d1
Victor Arteaga <xathovic@gmail.com>
2020-06-06 03:09:28 -0500

    Add User view and fix some comments

--- app/login/views.py
+++ app/login/views.py
@@ -1,16 +1,28 @@
 from rest_framework.response import Response
-from .models import Department, City
+from .models import User, Department, City
 from rest_framework import viewsets
-from .serializers import DepartmentSerializer, CitySerializer
+from .serializers import UserSerializer, DepartmentSerializer, CitySerializer
 from rest_framework.decorators import action
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+        API endpoints:
+        /api/users GET - Returns a list of all users
+        /api/users/<user_id> GET - Returns a single user identified by id or 404 if no match
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lockup_field = 'id'
+    http_method_names = ['get', 'head', 'post', 'put']
 
 
 class DepartmentViewSet(viewsets.ModelViewSet):
     """
         API endpoints:
-        /departments: GET - Returns a list of all departments
-        /departments/<id>: GET - Returns department filtered by id
-        /departments/<id>/cities: GET - Returns list of cities filtered by department id
+        /api/departments: GET - Returns a list of all departments
+        /api/departments/<id>: GET - Returns department filtered by id
+        /api/departments/<id>/cities: GET - Returns list of cities filtered by department id
     """
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
@@ -19,7 +31,6 @@ class DepartmentViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['GET'])
     def cities(self, request, pk):
-        print(request)
         department = self.get_object()
         cities = City.objects.filter(department=department)
         serializer = CitySerializer(cities, many=True)
@@ -29,7 +40,7 @@ class DepartmentViewSet(viewsets.ModelViewSet):
 class CityViewSet(viewsets.ModelViewSet):
     """
         API endpoints:
-        /cities: GET Return a list of all cities
+        /api/cities: GET Return a list of all cities
     """
     http_method_names = ['get', 'head']
     queryset = City.objects.all()
