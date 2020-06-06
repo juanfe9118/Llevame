c95503c19d0c37ca062745029723fc4805414d02
Victor Arteaga <xathovic@gmail.com>
2020-06-06 01:12:39 -0500

    Modify user admin panel

--- app/login/admin.py
+++ app/login/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Department, City
 
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Department)
-admin.site.register(City)
+class UserAdmin(BaseUserAdmin):
+    """ Define User admin panel """
+    list_display = ('first_name', 'last_name', 'type_id', 'n_document', 'email', 'date_joined', 'last_login', 'is_active')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('email',)
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, UserAdmin)
