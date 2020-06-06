ac43e303370d2894fd038fea7fbe67089ddbe6de
Victor Arteaga <xathovic@gmail.com>
2020-06-06 03:08:10 -0500

    Add user endpoint

--- app/login/urls.py
+++ app/login/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
 router.register(r'departments', views.DepartmentViewSet)
 router.register(r'cities', views.CityViewSet)
 
