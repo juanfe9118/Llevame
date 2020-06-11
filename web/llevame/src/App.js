70c648c7714dfd12e6ae19c9f655fdb867ea6c79
juanfe9118 <1168@holbertonschool.com>
2020-06-11 03:15:11 +0000

    Upload web feature branch

--- web/llevame/src/App.js
+++ web/llevame/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import HeaderLanding from './components/layout/HeaderLanding';
+import Footer from './components/layout/Footer';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' render={props => (
+            <React.Fragment>
+              <HeaderLanding />
+            </React.Fragment>
+          )} />
+          <Footer />
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
